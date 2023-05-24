3d5b22f7ea788cd5116a883c6ecdf59a626e0a9d
Sneha Bembalge <snehabembalge@gmail.com>
2023-05-24 23:11:54 +0530

    'data'

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+
+//Database connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+}).catch(err => {
+    console.log('Connection Failed...')
+});
 
 //set Template engine
 app.use(expressLayout)
@@ -13,21 +24,9 @@ app.set('view engine','ejs')
 //Assets
 app.use(express.static('public'))
 
-app.get('/',(req,res) =>{
-    res.render('home')
-})
+require('./routes/web')(app)
 
-app.get('/cart', (req,res) => {
-    res.render('customers/cart')
-})
 
-app.get('/login', (req,res) => {
-    res.render('auth/login')
-})
-
-app.get('/register', (req,res) => {
-    res.render('auth/register')
-})
 
 app.listen(PORT, () =>{
     console.log(`Listening on port ${PORT}`)
