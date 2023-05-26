329fb2768b10d4dd55ffbc82fc03f6ccf0bec725
Sneha Bembalge <snehabembalge@gmail.com>
2023-05-26 23:32:01 +0530

    'cart_functionality'

--- app/http/controllers/customers/cartController.js
+++ app/http/controllers/customers/cartController.js
@@ -3,6 +3,35 @@ function cartController() {
         index(req,res) {
             res.render('customers/cart')
 
+        },
+        update(req, res) {
+
+//for the first time creating cart and adding basic object structure
+            if(!req.session.cart) {
+                req.session.cart = {
+                    items: {},
+                    totalQty: 0,
+                    totalPrice: 0
+                }
+                
+            }
+            let cart = req.session.cart
+            
+                //check if iten does not exist in cart
+                if(!cart.items[req.body._id]) {
+                    cart.items[req.body._id] = {
+                        item: req.body,
+                        qty: 1
+                    }
+                    cart.toatlQty = cart.toatlQty + 1
+                    cart.totalPrice = cart.totalPrice + req.body.price
+                }else {
+                    cart.items[req.body._id].qty = cart.items[req.body._id].qty + 1
+                    cart.totalQty = cart.toatlQty + 1
+                    cart.totalPrice = cart.totalPrice + req.body.price
+                }
+
+            return res.json({ totalQty: req.session.cart.totalQty })
         }
     }
 }
--- app/http/controllers/homeController.js
+++ app/http/controllers/homeController.js
@@ -1,10 +1,13 @@
+const Menu = require('../../models/menu')
 function homeController() {
     return {
-        index(req,res) {
-            res.render('home')
+        async index(req,res) {
+           const pizzas = await Menu.find()
+                return res.render('home', { pizzas: pizzas})
 
+            }
+        
         }
     }
-}
 
 module.exports = homeController
