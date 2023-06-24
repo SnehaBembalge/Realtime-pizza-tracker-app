9d9fe4f4e60d5a4eac3beace884b37544e4e952a
Sneha Bembalge <snehabembalge@gmail.com>
2023-06-24 16:02:20 +0530

    'auth'

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbStore = require('connect-mongo')
+const passport = require('passport')
 
 //Database connection
 const url = 'mongodb://0.0.0.0/pizza';
@@ -29,6 +30,7 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology : true})
     console.log(err);
 })
 
+
 // Session store
 /*let mongoStore = new MongoDbStore({
         mongooseConnection: connection,
@@ -47,15 +49,25 @@ app.use(session({
 
 }))
 
+//Passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+//app.use(require('express-session')({ secret : 'keyboard cat', resave: true, saveUninitialized: true}))
+app.use(session({ secret : 'keyboard cat', resave: true, saveUninitialized: true}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 
 //Assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json( ))
 
 //Global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 
